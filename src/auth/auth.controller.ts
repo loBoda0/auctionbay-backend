aae487303b276296081706ec33f0a61fa8672008
Martin <martin.loboda02@gmail.com>
2023-12-18 17:59:07 +0100

    login user dto

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterUserDto } from './dto/register-user.dto';
 import { User } from 'src/entities/user.entity';
+import { LoginUserDto } from './dto/login-user.dto';
 
 @Controller('')
 export class AuthController {
@@ -11,6 +12,12 @@ export class AuthController {
   @HttpCode(HttpStatus.CREATED)
   async register(@Body() body: RegisterUserDto): Promise<User> {
     return this.authService.register(body)
+  }
+
+  @Post('login')
+  @HttpCode(HttpStatus.OK)
+  async login(@Body() body: LoginUserDto): Promise<User> {
+    return 
   }
 }
 
