60d0c9c66d435d5a7848d5b8a5ff4430bdec1137
Martin <martin.loboda02@gmail.com>
2023-12-18 17:05:01 +0100

    tmp

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,7 @@
 import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterUserDto } from './dto/register-user.dto';
-import { User } from 'src/entities/user.entity';
+import { User } from '../users/'
 
 @Controller('')
 export class AuthController {
