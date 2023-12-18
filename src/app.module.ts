e5ebc54cfcd0cd15ccc653c268530853af6e8784
Martin <martin.loboda02@gmail.com>
2023-12-18 17:36:13 +0100

    bug fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,6 @@ import { configValidationSchema } from './config/schema.config';
 import { DatabaseModule } from './database/database.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
-import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
